4877bd1ed9f8c0be16e931b53caf6f3e36e5b17e
Steve Halliwell <steve@animaginedreality.com>
2021-04-23 17:22:34 +1000

    Add comments and sample

--- Runtime/Injection/Injector.cs
+++ Runtime/Injection/Injector.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) AIR Pty Ltd. All rights reserved.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -11,6 +12,9 @@ namespace AIR.Flume
     {
         private const string INJECT = "Inject";
         private readonly FlumeServiceContainer _container;
+
+        private Dictionary<Type, InjectMethodAndArgumentsSet> _cachedInjectSets =
+            new Dictionary<Type, InjectMethodAndArgumentsSet>();
 
         public Injector(FlumeServiceContainer container)
         {
@@ -26,26 +30,59 @@ namespace AIR.Flume
                 return;
             }
 
-            var publicMethods = dependent.GetType().GetMethods();
-            var privateMethods = dependent.GetType().GetMethods(
+            var dependentType = dependent.GetType();
+
+            if (_cachedInjectSets.TryGetValue(dependentType, out var injectMethodAndArguments))
+            {
+                foreach (var injectMethod in injectMethodAndArguments)
+                {
+                    injectMethod.method.Invoke(dependent, injectMethod.args);
+                }
+                return;
+            }
+
+            var publicMethods = dependentType.GetMethods();
+            var privateMethods = dependentType.GetMethods(
                 BindingFlags.NonPublic | BindingFlags.Instance);
             var methods = publicMethods
                 .Concat(privateMethods)
                 .Distinct();
+
+            injectMethodAndArguments = new InjectMethodAndArgumentsSet();
+
             foreach (var injectMethod in methods) {
                 if (injectMethod.Name != INJECT) continue;
-                var typeDependencies = injectMethod
+                var injectArgTypes = injectMethod
                     .GetParameters()
                     .Select(p => p.ParameterType)
                     .ToArray();
 
                 var dependentServices = new List<object>();
-                foreach (var dependentType in typeDependencies) {
-                    var service = _container.Resolve(dependentType, dependent);
+                foreach (var injectArgType in injectArgTypes) {
+                    var service = _container.Resolve(injectArgType, dependent);
                     dependentServices.Add(service);
                 }
 
-                injectMethod.Invoke(dependent, dependentServices.ToArray());
+                var depArrayArgs = dependentServices.ToArray();
+                injectMethod.Invoke(dependent, depArrayArgs);
+
+                injectMethodAndArguments.Add(new InjectMethodAndArguments(injectMethod, depArrayArgs));
+            }
+
+            _cachedInjectSets[dependentType] = injectMethodAndArguments;
+        }
+
+        public class InjectMethodAndArgumentsSet : List<InjectMethodAndArguments> { }
+
+        public class InjectMethodAndArguments
+        {
+            public readonly MethodInfo method;
+            public readonly object[] args;
+
+            public InjectMethodAndArguments(MethodInfo method, object[] args)
+            {
+                this.method = method;
+                this.args = args;
             }
         }
     }
--- Runtime/Services/FlumeServiceContainer.cs
+++ Runtime/Services/FlumeServiceContainer.cs
@@ -5,15 +5,27 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+// TODO review and enforce all stylecop suggestions.
+
 namespace AIR.Flume
 {
     public class FlumeServiceContainer : MonoBehaviour
     {
-        public event Action<FlumeServiceContainer> OnContainerReady;
         private static Injector _injector;
+        private static Queue<IDependent> _earlyDependents = new Queue<IDependent>();
 
         private ServiceRegister _register = new ServiceRegister();
-        private static Queue<IDependent> _earlyDependents = new Queue<IDependent>();
+
+        public event Action<FlumeServiceContainer> OnContainerReady;
+
+        public static void InjectThis(Dependent dependent) =>
+            InjectThis(dependent as IDependent);
+
+        public static void InjectThis(DependentBehaviour dependentBehaviour) =>
+            InjectThis(dependentBehaviour as IDependent);
+
+        public static void InjectThis(ScriptableDependent dependentBehaviour) =>
+            InjectThis(dependentBehaviour as IDependent);
 
         public FlumeServiceContainer Register<TService>()
             where TService : class
@@ -33,42 +45,34 @@ namespace AIR.Flume
             where TService : class
             where TImplementation : TService
         {
-            if (typeof(TImplementation).IsSubclassOf(typeof(MonoBehaviour))) {
+            // TODO Needs to check for SO implementation too.
+            if (typeof(TImplementation).IsSubclassOf(typeof(MonoBehaviour)))
+            {
+                // TODO Doesn't need to find all only to then filter, unity can do that filter
                 Component monoBehaviour = FindObjectsOfType<MonoBehaviour>()
                     .FirstOrDefault(mb => mb is TImplementation);
                 if (monoBehaviour == null)
                     monoBehaviour = gameObject.AddComponent(typeof(TImplementation));
                 _register.Register(monoBehaviour as TService);
-            } else {
+            }
+            else
+            {
                 _register.Register<TService, TImplementation>();
             }
 
             return this;
         }
 
-        public static void InjectThis(Dependent dependent) =>
-            InjectThis(dependent as IDependent);
-
-        public static void InjectThis(DependentBehaviour dependentBehaviour) =>
-            InjectThis(dependentBehaviour as IDependent);
-
-        public static void InjectThis(ScriptableDependent dependentBehaviour) =>
-            InjectThis(dependentBehaviour as IDependent);
-
         internal object Resolve(Type dependentType, IDependent dependent)
         {
-            try {
+            try
+            {
                 return _register.Resolve(dependentType);
-            } catch (MissingServiceException) {
+            }
+            catch (MissingServiceException)
+            {
                 throw new MissingDependencyException(dependentType, dependent);
             }
-        }
-
-        private void OnDestroy()
-        {
-            _injector = null;
-            _register.Dispose();
-            _earlyDependents.Clear();
         }
 
         private static void InjectThis(IDependent dependentBehaviour)
@@ -77,6 +81,13 @@ namespace AIR.Flume
                 _earlyDependents.Enqueue(dependentBehaviour);
             else
                 _injector?.InjectDependencies(dependentBehaviour);
+        }
+
+        private void OnDestroy()
+        {
+            _injector = null;
+            _register.Dispose();
+            _earlyDependents.Clear();
         }
 
         private void Awake()
